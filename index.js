d377f6665d136a23ab31b657cdc5eebc58a643ab
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-05-31 12:34:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var request = require("request");
 
 function getDweetCode() {
 	return new Promise(function(resolve, reject) {
-		request("https://www.dwitter.net/api/dweets/?limit=1", function (error, response, body) {
-  			var code = JSON.parse(body).results.code
+		request("https://www.dwitter.net/api/dweets/?limit=1", function(error, response, body) {
+			var code = JSON.parse(body).results.code
 			resolve(code);
 		});
 	});
@@ -14,37 +14,37 @@ function getDweetCode() {
 
 function getFrames(cb) {
 	return new Promise(function(resolve, reject) {
-	getDweetCode().then(function(code) {
-		var framestouse = 50;
-		var can = new Canvas(1920, 1080);
-		var ctx = can.getContext("2d");
-		var frameArray = [];
-		var sandbox = {
-			S: Math.sin,
-			C: Math.cos,
-			T: Math.tan,
-			R: function(r, g, b, a) {
-				return "rgba("+r+","+g+","+b+","+a+")";
-			},
-			c: can,
-			x: ctx,
-			t: 0
-		};
-		var script = new vm.Script(code);
-		var context = vm.createContext(sandbox);
-		var timesaround = 0;
-		var intervalId = setInterval(function() {
-			if (timesaround < framestouse) {
-				script.runInContext(context);
-				frameArray.push(ctx.getImageData(0, 0, can.width, can.height).data);
-				context.t += 100;
-				timesaround += 1;
-			} else {
-				clearInterval(intervalId);
-				resolve(frameArray);
-			}
-		}, 100);
-	});
+		getDweetCode().then(function(code) {
+			var framestouse = 50;
+			var can = new Canvas(1920, 1080);
+			var ctx = can.getContext("2d");
+			var frameArray = [];
+			var sandbox = {
+				S: Math.sin,
+				C: Math.cos,
+				T: Math.tan,
+				R: function(r, g, b, a) {
+					return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+				},
+				c: can,
+				x: ctx,
+				t: 0
+			};
+			var script = new vm.Script(code);
+			var context = vm.createContext(sandbox);
+			var timesaround = 0;
+			var intervalId = setInterval(function() {
+				if (timesaround < framestouse) {
+					script.runInContext(context);
+					frameArray.push(ctx.getImageData(0, 0, can.width, can.height).data);
+					context.t += 100;
+					timesaround += 1;
+				} else {
+					clearInterval(intervalId);
+					resolve(frameArray);
+				}
+			}, 100);
+		});
 	});
 }
 
